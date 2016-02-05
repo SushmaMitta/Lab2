a44894c2436897e6396833c98fa04ca5812dd90a
SushmaMitta <sushma.mitta@gmail.com>
2016-02-04 22:46:53 -0600

    Lab2 Android Application Development

---
+++ Source/Text_to_Speech/app/src/main/java/com/sushmamitta/sushm/text_to_speech/TextToSpeechActivity.java
@@ -0,0 +1,64 @@
+package com.sushmamitta.sushm.text_to_speech;
+
+/**
+ * Created by sushm on 2/4/2016.
+ */
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Locale;
+import android.widget.Toast;
+
+public class TextToSpeechActivity extends Activity {
+    TextToSpeech t1;
+    EditText ed1;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_to_speech);
+        ed1=(EditText)findViewById(R.id.editText);
+        b1=(Button)findViewById(R.id.button);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = ed1.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_text_to_speech, menu);
+        return true;
+    }
+
+
+}
